86cdea6b798285218ae1a31adc5b88bd41785c9c
Jojo Vanhoecke <72917013+JojoVh@users.noreply.github.com>
2025-06-24 11:18:39 +0200

    Update set_firstsample.m

--- set_firstsample.m
+++ set_firstsample.m
@@ -2,10 +2,10 @@ function firstsample = set_firstsample(string_of_TicksInMses)
 
     parts = strsplit(string_of_TicksInMses, ',');
 
-    % extract the first part and convert it to a number, divide by 50ms
+    % extract the first part and convert it to a number
 
-    firstsample = str2num(parts{1})/50;
-    
+    firstsample = str2num(parts{1});
+    warning('firstsample is no longer divided by 50')
     if isempty(firstsample)
         firstsample=1;
     end
