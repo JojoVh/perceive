5f6922599ec87b78d7eda39ec14c76be4fd297c7
Jojo Vanhoecke <72917013+JojoVh@users.noreply.github.com>
2026-02-10 17:39:51 +0100

    Major Update: Time and realtime
    d.time is henceforth d.time{1}(e) = (cdata.LfpData(e).TicksInMs - cdata.LfpData(1).TicksInMs) / 1000;
    
    instead of
    
    %d.time{1}(e) = seconds(datetime(runs{c},'InputFormat','yyyy-MM-dd HH:mm:ss.SSS') - hdr.d0) ...
            %              + ((cdata.LfpData(e).TicksInMs/1000) - tstart_TicksInMs);
            %% check the time: subtraction of hdr.d0 should not be necessary and could lead to errors with js.AbnormalEnd = true
    
    plus update of the mod numbers, now by json element
    
    The realtime is calcuated based on the FirstPacketDateTime and MTicks

--- toolbox/perceive_extract_bsl.m
+++ toolbox/perceive_extract_bsl.m
@@ -1,4 +1,4 @@
-function [alldata, counterBSL] = perceive_extract_bsl(data, hdr)
+function [alldata, list_of_BSL] = perceive_extract_bsl(data, hdr)
 
 % extracts BrainSense LFP data into FieldTrip-compatible structures
 %
@@ -12,12 +12,21 @@ function [alldata, counterBSL] = perceive_extract_bsl(data, hdr)
 
 alldata = {};
 counterBSL = 0;
+list_of_BSL = {};
 
-FirstPacketDateTime = strrep(strrep({data(:).FirstPacketDateTime},'T',' '),'Z','');
-runs = unique(FirstPacketDateTime);
+runs_FirstPacketDateTime = strrep(strrep({data(:).FirstPacketDateTime},'T',' '),'Z','');
+assert(isequal(runs_FirstPacketDateTime , unique(runs_FirstPacketDateTime)));
+if ~isequal(length(runs_FirstPacketDateTime), length(data))
+    error('Amount of unique FirstPackageTimeStamps (runs = %d) is not the same as amount of data entries (data = %d) in BrainSenseLFP.', ...
+        length(runs_FirstPacketDateTime), length(data));
+    % How to solve this issue: make sure that the iterator c goes over all
+    % FirstPacketDateTimes and is not allocating wrong data to cdata.
+    % Have c = 1:length(data) instead.
+end
 
-for c = 1:length(runs)
-    cdata = data(c);
+for js_element = 1:length(runs_FirstPacketDateTime)
+
+    cdata = data(js_element);
     tmp = strrep(cdata.Channel,'_AND','');
     tmp = strsplit(strrep(strrep(strrep(strrep(strrep(tmp,'ZERO','0'),'ONE','1'),'TWO','2'),'THREE','3'),'_',''),',');
 
@@ -32,6 +41,7 @@ for c = 1:length(runs)
 
     d = [];
     d.hdr = hdr;
+    d.FirstPacketDateTime = runs_FirstPacketDateTime{js_element};
     d.hdr.BSL.TherapySnapshot = cdata.TherapySnapshot;
     lfpsettings = cell(2,1);
     stimchannels = cell(2,1);
@@ -95,24 +105,56 @@ for c = 1:length(runs)
     d.fsample = cdata.SampleRateInHz;
     d.hdr.Fs = d.fsample;
 
-    tstart = cdata.LfpData(1).TicksInMs / 1000;
+    tstartInSecs_of_TicksInMs = cdata.LfpData(1).TicksInMs / 1000;
     for e = 1:length(cdata.LfpData)
         d.trial{1}(1:2,e) = [cdata.LfpData(e).Left.LFP; cdata.LfpData(e).Right.LFP];
         d.trial{1}(3:4,e) = [cdata.LfpData(e).Left.mA; cdata.LfpData(e).Right.mA];
-        d.time{1}(e) = seconds(datetime(runs{c},'InputFormat','yyyy-MM-dd HH:mm:ss.SSS') - hdr.d0) ...
-                      + ((cdata.LfpData(e).TicksInMs/1000) - tstart);
-        d.realtime(e) = datetime(runs{c},'Inputformat','yyyy-MM-dd HH:mm:ss.SSS','Format','yyyy-MM-dd HH:mm:ss.SSS') ...
-                      + seconds(d.time{1}(e) - d.time{1}(1));
+        d.time{1}(e) = seconds(timeofday(datetime(runs_FirstPacketDateTime{js_element}))) + ...
+               ((cdata.LfpData(e).TicksInMs/1000) - tstartInSecs_of_TicksInMs);
+        d.time{1}(e) = (cdata.LfpData(e).TicksInMs - cdata.LfpData(1).TicksInMs) / 1000; %this is a cleaner solution
+        
+        % OLD
+        %d.time{1}(e) = seconds(datetime(runs{c},'InputFormat','yyyy-MM-dd HH:mm:ss.SSS') - hdr.d0) ...
+        %              + ((cdata.LfpData(e).TicksInMs/1000) - tstart_TicksInMs);
+        %% check the time: subtraction of hdr.d0 should not be necessary and could lead to errors with js.AbnormalEnd = true
+        % hdr.d0 = datetime(js.DeviceInformation.Final.DeviceDateTime(1:10))
+            % or in case of errors
+        % hdr.0 = datetime(js.SessionEndDate(1:10))
+            % see further perceive_extract_hdr
+        % t = datetime(runs{c}, 'InputFormat', 'yyyy-MM-dd HH:mm:ss.SSS');
+        % timeOnly = timeofday(t);
+        % secondsValue = seconds(timeOnly);
+        % %assert(isequal(secondsValue,seconds(datetime(runs{c},'InputFormat','yyyy-MM-dd HH:mm:ss.SSS') - hdr.d0)))
+        % d_run = datetime(runs{c}(1:10));   % nur YYYY-MM-DD
+        % d_hdr = dateshift(hdr.d0, 'start', 'day');
+        % 
+        % if ~isequal(d_run, d_hdr)
+        %     error('Header:SessionDateMismatch', ...
+        %         ['SessionDate and SessionEndDate do not match the ', ...
+        %         'FirstPackageDateTime of this run.\n', ...
+        %         'FirstPackage date: %s\n', ...
+        %         'hdr.d0 date:       %s'], ...
+        %         char(d_run), char(d_hdr));
+        % end
+
+        %%
+        d.realtime(e) = datetime(runs_FirstPacketDateTime{js_element},'InputFormat','yyyy-MM-dd HH:mm:ss.SSS','Format','yyyy-MM-dd HH:mm:ss.SSS') ...
+                + seconds(d.time{1}(e)) - seconds(d.time{1}(1));
+
+        d.TicksInMs(e) = cdata.LfpData(e).TicksInMs; %this is the TicksInMs and is a solid alternative, as it is not (re)calculated, but raw data from js
         d.hdr.BSL.seq(e) = cdata.LfpData(e).Seq;
     end
 
-    d.trialinfo(1) = c;
+    d.trialinfo(1) = js_element;
     d.hdr.realtime = d.realtime;
 
     counterBSL = counterBSL + 1;
-    mod = 'mod-BSL';
+    js_element_nr = js_element-1;
+    mod = ['mod-BSL' num2str(js_element_nr)];
     d.fname = [hdr.fname '_' mod];
-    d.fname = strrep(d.fname, 'task-Rest', ['task-TASK' num2str(counterBSL)]);
+    %d.fname = strrep(d.fname, 'task-Rest', ['task-TASK' num2str(counterBSL)]);
+    d.fname = strrep(d.fname, 'task-Rest', 'task-TASK');
+    list_of_BSL{end+1} = d.fname;
 
     if contains(d.label{3}, 'STIM_L')
         LAmp = d.trial{1}(3,:);
@@ -137,7 +179,7 @@ for c = 1:length(runs)
     assert(ischar(acq));
 
     d.fname = perceive_updateAcq(d.fname, acq);
-    d.fnamedate = char(datetime(runs{c},'Inputformat','yyyy-MM-dd HH:mm:ss.SSS','format','yyyyMMddhhmmss'));
+    d.fnamedate = char(datetime(runs_FirstPacketDateTime{js_element},'Inputformat','yyyy-MM-dd HH:mm:ss.SSS','format','yyyyMMddhhmmss'));
 
     alldata{end+1} = d;
 
